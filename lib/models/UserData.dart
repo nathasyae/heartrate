f79e06f8517196bd508fc8d76d677a6cf3845835
nathasyae <elioranathasya@gmail.com>
2021-03-22 17:08:11 +0700

    userdata model

--- lib/models/UserData.dart
+++ lib/models/UserData.dart
@@ -1,37 +1,66 @@
-class UserData {
-  final String uid;
-  double weight, weightUnit, height, heightUnit;
-  int genderIdx, healthConditionIdx, cvdIllnessIdx, alcoholStatusIdx, cigarretesStatusIdx;
+import 'dart:core';
 
-  UserData(
-      this.uid,
-      this.weight,
-      this.weightUnit,
-      this.height,
-      this.heightUnit,
-      this.genderIdx,
-      this.healthConditionIdx,
-      this.cvdIllnessIdx,
-      this.alcoholStatusIdx,
-      this.cigarretesStatusIdx,
-      );
+class UserData{
+  String uid, gender, activity_level;
+  double weight, height;
+  List<String> health_conditions, cvd_history;
 
-  double getWeight(){
-    return this.weight;
+  UserData({this.uid, this.gender, this.activity_level, this.weight, this.height, this.health_conditions, this.cvd_history});
+
+  String getUid(){
+    return this.uid;
+  }
+
+  void setUid(String uid){
+    this.uid = uid;
+  }
+
+  String getGender(){
+    return this.gender;
+  }
+
+  void setGender(String gender){
+    this.gender = uid;
+  }
+
+  String getActivityLevel(){
+    return this.activity_level;
+  }
+
+  void setActivityLevel(String activity_level){
+    this.activity_level = activity_level;
   }
 
   double getHeight(){
     return this.height;
   }
 
+  void setHeight(double height){
+    this.height = height;
+  }
 
+  double getWeight(){
+    return this.weight;
+  }
 
+  void setWeight(double weight){
+    this.weight = weight;
+  }
+  
+  List<String> getHealthConditions(){
+    return health_conditions;
+  }
 
-  // Map<String, dynamic> toJson() => {
-  //   'title': title,
-  //   'startDate': startDate,
-  //   'endDate': endDate,
-  //   'budget': budget,
-  //   'travelType': travelType,
-  // };
+  void setHealthConditions(List<String> health_conditions){
+    this.health_conditions = health_conditions;
+  }
+
+  List<String> getCvdHistory(){
+    return cvd_history;
+  }
+
+  void setCvdHistory(List<String> cvd_history){
+    this.cvd_history = cvd_history;
+  }
+
 }
